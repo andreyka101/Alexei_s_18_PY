7eb45fd51c13fd63457928669966dd9c37cc5992
andreyka101 <andrejkvasnin236@gmail.com>
2024-10-26 14:22:28 +0300

    first commit

---
+++ lesson_1_numder.py
@@ -0,0 +1,48 @@
+# print('hello world')
+# print("hello world")
+# print(21)
+
+
+
+# num = 5
+# print(num)
+
+
+
+# num_2 =  9
+# print(num_2)
+# num_2 = 0
+# print(num_2)
+
+
+
+# print(4.5)
+# print(2 , "     " , 5 , "qwerty" , 0.1)
+# print("qwertyu\niopasdf")
+
+
+
+# print("2 + 2 =" , 2 + 2)
+# print("2 - 2 =" , 2 - 2)
+# print("2 * 3 =" , 2 * 3)
+# print("2 ** 3 =" , 2 ** 3)
+# print("10 / 3 =" , 10 / 3)
+# print("10 // 3 =" , 10 // 3)
+# print("10 % 3 =" , 10 % 3)
+
+
+
+# num_3 = 4
+# print(num_3)
+# num_3 = num_3 * 3
+# print(num_3)
+
+
+
+qwerty = 5
+hello_world = 5
+helloWorld = 5
+qw4ert5 = 8
+
+
+
